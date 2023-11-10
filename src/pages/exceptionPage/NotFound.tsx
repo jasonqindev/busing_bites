ad041e53e601feca6fa0e1503ef990716c983c6d
Gaolong Qin <GAOLONG.QIN.2022@MUMAIL.IE>
2023-11-10 02:22:18 +0000

    404 page

--- src/pages/exceptionPage/NotFound.tsx
+++ src/pages/exceptionPage/NotFound.tsx
@@ -1,7 +1,12 @@
-import { FC } from 'react';
+import { FC } from "react";
+import styles from "./exception.module.scss";
 
 const NotFound: FC = () => {
-  return <p>NotFound</p>;
+  return (
+    <div className={styles.container}>
+      <img src="/images/404.jpg" alt="404" />
+    </div>
+  );
 };
 
 export default NotFound;
